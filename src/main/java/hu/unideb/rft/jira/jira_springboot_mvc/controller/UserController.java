a2e7ba2b5d87c79bac0273d97027dfee3b2782d5
parbajhos1996 <adee.48@gmail.com>
2017-11-05 12:00:28 +0100

    Task table & login check

--- src/main/java/hu/unideb/rft/jira/jira_springboot_mvc/controller/UserController.java
+++ src/main/java/hu/unideb/rft/jira/jira_springboot_mvc/controller/UserController.java
@@ -1,9 +1,12 @@
 package hu.unideb.rft.jira.jira_springboot_mvc.controller;
 
 import hu.unideb.rft.jira.jira_springboot_mvc.email.RegistrationEmailThread;
+import hu.unideb.rft.jira.jira_springboot_mvc.entity.Task;
 import hu.unideb.rft.jira.jira_springboot_mvc.entity.User;
 import hu.unideb.rft.jira.jira_springboot_mvc.service.SecurityService;
+import hu.unideb.rft.jira.jira_springboot_mvc.service.TaskService;
 import hu.unideb.rft.jira.jira_springboot_mvc.service.UserService;
+import hu.unideb.rft.jira.jira_springboot_mvc.validator.TaskValidator;
 import hu.unideb.rft.jira.jira_springboot_mvc.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -15,10 +18,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.security.Principal;
+
 @Controller
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private TaskService taskService;
 
     @Autowired
     private SecurityService securityService;
@@ -27,14 +35,20 @@ public class UserController {
     private UserValidator userValidator;
 
     @Autowired
+    private TaskValidator taskValidator;
+
+    @Autowired
     private ThreadPoolTaskExecutor threadPoolTaskExecutor;
 
     @Autowired
     private ApplicationContext applicationContext;
 
     @RequestMapping(value = "/signup", method = RequestMethod.GET)
-    public String registration(Model model) {
+    public String registration(Model model , Principal user) {
         model.addAttribute("userForm", new User());
+
+        if (user != null)
+            return "redirect:/welcome";
 
         return "signup";
     }
@@ -60,19 +74,35 @@ public class UserController {
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(Model model, String error, String logout) {
+    public String login(Model model, String error, String logout,Principal user) {
         if (error != null)
             model.addAttribute("error", "Your username and password is invalid.");
 
         if (logout != null)
             model.addAttribute("message", "You have been logged out successfully.");
 
+        if (user != null)
+            return "redirect:/welcome";
+
         return "login";
     }
 
     @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
-    public String welcome(Model model) {
+    public String welcome(Model model , Principal user) {
         return "welcome";
+    }
+
+    @RequestMapping(value = {"/tasktest"}, method = RequestMethod.GET)
+    public String tasktest(Model model,Principal user) {
+        //Task table test
+        Task a= new Task();
+        a.setTaskname("valami");
+        a.setBesorolas("bug");
+        a.setSkalazas(1);
+        a.setUsername(user.getName());
+        a.setUser(userService.findByUsername(user.getName()));
+        taskService.save(a);
+        return "tasktest";
     }
 
 }
--- src/main/java/hu/unideb/rft/jira/jira_springboot_mvc/entity/User.java
+++ src/main/java/hu/unideb/rft/jira/jira_springboot_mvc/entity/User.java
@@ -15,6 +15,7 @@ public class User {
     private String password;
     private String passwordConfirm;
     private Set<Role> roles;
+    private Set<Task> task;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -84,4 +85,12 @@ public class User {
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
     }
+
+    @OneToMany
+    @JoinTable(name = "user_task", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "task_id"))
+    public Set<Task> getTask() {
+        return task;
+    }
+
+    public void setTask(Set<Task> task) { this.task = task; }
 }
