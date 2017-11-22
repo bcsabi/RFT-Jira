3c0c9c93fa5d1cada844694b89ab3795b67c137d
bcsabi <csabesz152@gmail.com>
2017-11-22 02:05:36 +0100

    fixed display on backlog page

--- src/main/webapp/resources/js/backlog.js
+++ src/main/webapp/resources/js/backlog.js
@@ -6,10 +6,12 @@ $(document).ready(function() {
             $('.new_task_button').css("background-color", "#3cb0fd");
             $('.new_task_button').css("border", "1px solid #3cb0fd");
             $('.tasks').css('display','none');
+            $('.modify_task').css('display', 'none');
         }
         else {
             $('.new_task').css('display', 'none');
-            $('.tasks').css('display','block');
+            $('.tasks').css('display','inline-block');
+            $('.modify_task').css('display', 'inline-block');
             $('.new_task_button').css("background-color", "#4CAF50");
             $('.new_task_button').css("border", "1px solid #4CAF50");
         }
